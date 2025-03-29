ab3a0ae85eb3ebcb8bf3314b728a5f0840818bf0
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-30 05:01:04 +0600

    no worry

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/eid-greetings-boro-via/",
   plugins: [react(), tailwindcss()],
 })
